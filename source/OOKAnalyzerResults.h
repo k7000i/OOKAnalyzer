7250ce29e0228a7d9c5f8aef47ae7475c871ae9d
raczattila <kelmen.zoltan@gmail.com>
2020-07-08 19:54:26 +0200

    initial commit

--- source/OOKAnalyzerResults.h
+++ source/OOKAnalyzerResults.h
@@ -1,16 +1,16 @@
-#ifndef SIMPLESERIAL_ANALYZER_RESULTS
-#define SIMPLESERIAL_ANALYZER_RESULTS
+#ifndef OOK_ANALYZER_RESULTS
+#define OOK_ANALYZER_RESULTS
 
 #include <AnalyzerResults.h>
 
-class SimpleSerialAnalyzer;
-class SimpleSerialAnalyzerSettings;
+class OOKAnalyzer;
+class OOKAnalyzerSettings;
 
-class SimpleSerialAnalyzerResults : public AnalyzerResults
+class OOKAnalyzerResults : public AnalyzerResults
 {
 public:
-	SimpleSerialAnalyzerResults( SimpleSerialAnalyzer* analyzer, SimpleSerialAnalyzerSettings* settings );
-	virtual ~SimpleSerialAnalyzerResults();
+	OOKAnalyzerResults( OOKAnalyzer* analyzer, OOKAnalyzerSettings* settings );
+	virtual ~OOKAnalyzerResults();
 
 	virtual void GenerateBubbleText( U64 frame_index, Channel& channel, DisplayBase display_base );
 	virtual void GenerateExportFile( const char* file, DisplayBase display_base, U32 export_type_user_id );
@@ -22,8 +22,8 @@ public:
 protected: //functions
 
 protected:  //vars
-	SimpleSerialAnalyzerSettings* mSettings;
-	SimpleSerialAnalyzer* mAnalyzer;
+	OOKAnalyzerSettings* mSettings;
+	OOKAnalyzer* mAnalyzer;
 };
 
-#endif //SIMPLESERIAL_ANALYZER_RESULTS
+#endif //OOK_ANALYZER_RESULTS
